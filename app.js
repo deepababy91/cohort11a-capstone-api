f0fd5bd842615df8069e101b3495381aa3b72062
Deepa Baby <deepababy91@gmail.com>
2021-10-10 21:57:24 -0500

    checking in

--- app.js
+++ app.js
@@ -48,5 +48,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.listen(process.env.PORT||PORT,()=>{
+  console.log(`Server running on PORT ${PORT}`);
+})
 
 module.exports = app;
