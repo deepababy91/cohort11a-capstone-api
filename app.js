a63c66c2b30038789dc7a89d7b7ca76e6f1bdff8
Deepa Baby <deepababy91@gmail.com>
2021-12-24 19:52:55 -0600

    adding port

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+//let port=process.env.PORT;
 let port=process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log(`App is running at the port ${port}`)
