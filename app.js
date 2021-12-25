09bf1d7635560290a1faf1671064fb22969ac391
Deepa Baby <deepababy91@gmail.com>
2021-12-25 11:26:06 -0600

    checking in

--- app.js
+++ app.js
@@ -55,13 +55,29 @@ if(process.env.NODE_ENV==='production'){
     res.sendFile(path.join(__dirname,'client/build','index.html'));//relative apth
   })
 }
-app.use(function (req, res, next) {
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://cohort-capstone-api.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+/*app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
- });
+ });*/
 //let port=process.env.PORT;
 let port=process.env.PORT || 3000;
 app.listen(port, ()=>{
