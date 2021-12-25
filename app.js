026a9e0823d439b07cb859de3236ba0183329f22
Deepa Baby <deepababy91@gmail.com>
2021-12-25 11:37:37 -0600

    checking in

--- app.js
+++ app.js
@@ -71,13 +71,14 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-/*app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
- });*/
+};
+
+app.use(allowCrossDomain);
 //let port=process.env.PORT;
 let port=process.env.PORT || 3000;
 app.listen(port, ()=>{
