2387005c94a92131425c865a36845b9a0058cf40
Deepa Baby <deepababy91@gmail.com>
2021-12-25 00:42:11 -0600

    checking in

--- app.js
+++ app.js
@@ -55,6 +55,13 @@ if(process.env.NODE_ENV==='production'){
     res.sendFile(path.join(__dirname,'client/build','index.html'));//relative apth
   })
 }
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+ });
 //let port=process.env.PORT;
 let port=process.env.PORT || 3000;
 app.listen(port, ()=>{
